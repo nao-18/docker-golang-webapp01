c0ef2232fc67defdead5ae78b9255ac331208d18
nao18 <justdo.nao01@gmail.com>
2021-03-08 17:21:29 +0000

    create the simple server

--- src/main.go
+++ src/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+const posrNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the about oage handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the abount page add 2 + 2 is %d", sum))
+}
+
+// addValues adds two integers and return the sum
+func addValues(x, y int) int {
+	return x + y
+}
+
+// main is the main application function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", posrNumber))
+	http.ListenAndServe(posrNumber, nil)
 }
