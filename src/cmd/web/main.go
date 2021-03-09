df98cdb3de9803a75fa336bb63faeb56c0412d80
nao18 <justdo.nao01@gmail.com>
2021-03-09 12:46:41 +0000

    main.go file spilit

--- src/cmd/web/main.go
+++ src/cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/nao18/docker-golang-webapp01/src/pkg/handlers"
 )
 
 const posrNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", posrNumber))
 	http.ListenAndServe(posrNumber, nil)
